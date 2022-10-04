ff02279671d77394e7c74f0ba854a3e11fe58ea4
Anudeep <you@example.com>
2022-10-04 17:53:54 +0530

    prepare v1 deploy

---
+++ src/app/store.ts
@@ -0,0 +1,21 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import bookReducer from "../features/books/bookSlice";
+import userReducer from "../features/user/userSlice";
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    book: bookReducer,
+    user: userReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
